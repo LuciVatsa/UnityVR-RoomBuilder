780c4ab89cbc3863976253eca24c3d5756edaca2
Saaransh Vatsa <u1257247@utah.edu>
2020-01-15 16:55:58 -0700

    Added Light Switch Assets and Light Switch Script, Tested using moving object

---
+++ Room Builder/Assets/LightSwitch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class LightSwitch : MonoBehaviour
+{
+    public GameObject Light;
+    private bool LightState = true;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(LightState)
+        {
+            Light.SetActive(false);
+            LightState = false;
+        }
+        else
+        {
+            Light.SetActive(true);
+            LightState = true;
+        }
+        
+    }
+}
+               
+
+
