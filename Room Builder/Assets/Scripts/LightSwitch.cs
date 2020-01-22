330f4e56d42ba6965ab64eb7a0eb74b4bbbd194f
LuciVatsa <saaranshvatsa@yahoo.in>
2020-01-22 16:54:25 -0700

    Trying to go Back

--- Room Builder/Assets/Scripts/LightSwitch.cs
+++ Room Builder/Assets/Scripts/LightSwitch.cs
@@ -5,19 +5,31 @@ using UnityEngine;
 
 public class LightSwitch : MonoBehaviour
 {
-    public GameObject Light;
+    public GameObject[] Lights;
     private bool LightState = true;
+    private GameObject Fade;
+    void Start()
+    {
+        Fade = GetComponent<TurnOffLight>();
+    }
     public void TurnLight()
     {
         Debug.Log("I was here");
+
         if(LightState)
         {
-            Light.SetActive(false);
+            foreach(GameObject light in Lights)
+            {
+                light.SetActive(false);
+            }
             LightState = false;
         }
         else
         {
-            Light.SetActive(true);
+            foreach (GameObject light in Lights)
+            {
+                light.SetActive(true);
+            }
             LightState = true;
         }
         
