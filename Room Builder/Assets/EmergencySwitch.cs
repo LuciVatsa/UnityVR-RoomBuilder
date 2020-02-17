7fc1bf7247b2b787768c4944ce4478643b08160e
LuciVatsa <saaranshvatsa@yahoo.in>
2020-02-17 16:42:27 -0700

    Made Props, Now ready to populate the rooms

---
+++ Room Builder/Assets/EmergencySwitch.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmergencySwitch : MonoBehaviour
+{
+    public AudioSource SosAudio;
+
+    bool bIsSwitchOn = false;
+
+    public void SOSTime()
+    {
+        SosAudio.loop = true;
+        if (!bIsSwitchOn)
+        {
+            SosAudio.Play();
+            bIsSwitchOn = true;
+        }
+        else if (bIsSwitchOn)
+        {
+            SosAudio.Stop();
+            bIsSwitchOn = false;
+
+        }
+    }
+}
