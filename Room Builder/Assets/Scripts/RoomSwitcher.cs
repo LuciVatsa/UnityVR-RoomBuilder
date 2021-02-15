e1f03ac2e7916b5c5021e41f6e12186d66dae0a5
LuciVatsa <saaranshvatsa@yahoo.in>
2021-02-15 12:13:24 -0700

    Changed Room Layout and added proximity triggers

---
+++ Room Builder/Assets/Scripts/RoomSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomSwitcher : MonoBehaviour
+{
+    public Transform Player;
+    public Transform RoomLocation;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Room Check");
+        Player.position = RoomLocation.position;
+    }
+}
