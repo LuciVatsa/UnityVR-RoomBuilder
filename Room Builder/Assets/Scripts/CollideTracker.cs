fb1d257306e002dc2544514806c9ecd37dcfb02a
LuciVatsa <saaranshvatsa@yahoo.in>
2021-11-16 15:55:11 -0700

    [Anna] upload data to google sheet / modified IV prefab

--- Room Builder/Assets/Scripts/CollideTracker.cs
+++ Room Builder/Assets/Scripts/CollideTracker.cs
@@ -7,11 +7,12 @@ using System.Collections;
 public class CollideTracker : MonoBehaviour
 {
     float startTime, endTime;
-    //search "form action" in after right click and select "view page source"
-    [SerializeField]
-    private string BASE_URL = "https://docs.google.com/forms/u/1/d/e/1FAIpQLSewEczoXrGsfBbS_5ByJ7T6bMgo6rKQAHRPjZl6FmAM7DEpOA/formResponse";
+    /*search "form action" in after right click and select "view page source"*/
+    //[SerializeField]
+    //private string BASE_URL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSewEczoXrGsfBbS_5ByJ7T6bMgo6rKQAHRPjZl6FmAM7DEpOA/formResponse";
+    private string BASE_URL = "https://docs.google.com/forms/u/1/d/e/1FAIpQLScwb_zNFMZLWFpufV5x1mm41cfcYPQM4dHPo5JaBuvXuELcUw/formResponse";
 
-
+    
 /*
  * Trigger enter works, but player's hand need to add rigidbody and isTrigger need to be enable
  * Also, this scripts need to be attach to every object that can be collided
@@ -30,9 +31,10 @@ public class CollideTracker : MonoBehaviour
         float totalTime = endTime - startTime;
         //Debug.Log("Exit! time: " + endTime.ToString());
         Debug.Log("\nName: " + name + ", Start: " +  startTime.ToString() + ", End: " +  endTime.ToString() + ", Total: " +  totalTime.ToString()+ ", Collision Object: " + collision.gameObject);
-        //StartCoroutine(Post(name, startTime.ToString(), endTime.ToString(), totalTime.ToString()));
+        StartCoroutine(Post(collision.gameObject.ToString(), name, startTime.ToString(), endTime.ToString(), totalTime.ToString()));
     }
 
+    /*
     void OnTriggerEnter()
     {
         startTime = Time.time;
@@ -46,16 +48,25 @@ public class CollideTracker : MonoBehaviour
         float totalTime = endTime - startTime;
         Debug.Log("Trigger Exit! time: " + endTime.ToString());
 
-        //StartCoroutine(Post(name, startTime.ToString(), endTime.ToString(), totalTime.ToString());
+        StartCoroutine(Post(name, startTime.ToString(), endTime.ToString(), totalTime.ToString());
     }
+    */
 
-    IEnumerator Post(string i_name, string i_startTime, string i_endTime, string i_totalTime)
+    IEnumerator Post(string i_playerContactName, string i_name, string i_startTime, string i_endTime, string i_totalTime)
     {
         WWWForm form = new WWWForm();
+        /*
+        form.AddField("entry.1156870628", i_playerContactName);
         form.AddField("entry.1219406179", i_name);
         form.AddField("entry.1375789603", i_startTime);
         form.AddField("entry.1681171193", i_endTime);
-        form.AddField("entry.1523515611", i_totalTime);
+        form.AddField("entry.1523515611", i_totalTime);*/
+
+        form.AddField("entry.1700357637", i_playerContactName);
+        form.AddField("entry.1172557804", i_name);
+        form.AddField("entry.1173118163", i_startTime);
+        form.AddField("entry.343679423", i_endTime);
+        form.AddField("entry.1413142285", i_totalTime);
 
         byte[] rawDataGoogle = form.data;
         WWW www = new WWW(BASE_URL, rawDataGoogle);
