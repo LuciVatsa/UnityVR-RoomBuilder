3571c223247c472bc0f368356f667fa24a5d364a
Anna Chen <53270829+anna-chestnut@users.noreply.github.com>
2022-02-22 14:23:28 -0700

    Menu manager

---
+++ Room Builder/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+public class MenuManager : MonoBehaviour
+{
+    private static Dictionary<string, MonoScript> AllScripts = new Dictionary<string, MonoScript>();
+
+    [MenuItem("VR/Enable Data Scripts")]
+    static void EnableScripts()
+    {
+        AllScripts.Clear();
+        UnityEngine.Object[] scripts = Resources.LoadAll("Scripts");
+
+        foreach (UnityEngine.Object script in scripts)
+        {
+            if (scripts.ToString().Contains("Tracker"))
+            {
+                scripts.;
+            }
+        }
+
+        Debug.Log(AllScripts.Count.ToString());
+    }
+}
