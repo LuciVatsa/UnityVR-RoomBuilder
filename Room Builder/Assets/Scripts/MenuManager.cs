f66559f31fe41563eec972364e71c78768eb9da9
LuciVatsa <saaranshvatsa@yahoo.in>
2022-02-23 16:59:04 -0700

    Update MenuManager.cs

--- Room Builder/Assets/Scripts/MenuManager.cs
+++ Room Builder/Assets/Scripts/MenuManager.cs
@@ -4,13 +4,39 @@ using UnityEditor;
 using UnityEngine;
 
 
-public class MenuManager : MonoBehaviour
+public class MenuManager : Editor
 {
     private static Dictionary<string, MonoScript> AllScripts = new Dictionary<string, MonoScript>();
+    List<Component> _scriptsList = new List<Component>();
+
+    
 
     [MenuItem("VR/Enable Data Scripts")]
     static void EnableScripts()
     {
+        GameObject[] gameobjects;
+        gameobjects = GameObject.FindGameObjectsWithTag("DataCollect");
+        //var obj = GameObject.FindGameObjectWithTag("DataCollect");
+        var head = GameObject.FindGameObjectWithTag("MainCamera");
+
+        foreach(var go in gameobjects)
+        {
+            foreach (MonoBehaviour script in go.GetComponents<MonoBehaviour>())
+            {
+                if (script.name.Contains("Tracker"))
+                    script.enabled = true;
+            }
+        }
+
+        string[] assetPaths = AssetDatabase.GetAllAssetPaths();
+        foreach (string assetPath in assetPaths)
+        {
+            if (assetPath.Contains(".cs")) // or .js if you want
+            {
+                Debug.Log(assetPath);
+            }
+        }
+
         AllScripts.Clear();
         UnityEngine.Object[] scripts = Resources.LoadAll("Scripts");
 
@@ -18,7 +44,7 @@ public class MenuManager : MonoBehaviour
         {
             if (scripts.ToString().Contains("Tracker"))
             {
-                scripts.;
+                
             }
         }
 
